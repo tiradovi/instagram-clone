17de60c45f57a393d41f0809068149237b55c8f1
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-12-18 12:54:31 +0900

    카카오 수정

--- front-end/src/pages/SignupPage.jsx
+++ front-end/src/pages/SignupPage.jsx
@@ -47,7 +47,7 @@ const SignupPage = () => {
             return;
         }
 
-        if (password.length < 6) {
+        if (!isKakaoSignup &&password.length < 6) {
             alert('비밀번호는 최소 6자 이상이어야 합니다.');
             return;
         }
